0e53c99c500ec07688b58f47e0853a7d1168de8e
luizpaulofranz <luizpaulofranz@gmail.com>
2018-05-25 16:19:02 -0300

    Adds graphQl, apollo server and body-parser dependencies.
    Map the server endpoint (from apollo, which uses graphQl dependencie) and a test client endpoint (graphiql, which is a visual client to mount graphql queries, from apollo too).

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 import express from 'express';
+// graphqlExpress is the glue layer between express and graphql server
+// graphiqlExpress is a visual query editor to perform queries in graphql server (graphQl client)
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
+// parse requests body to json is needed for graphql
+import bodyParser from 'body-parser';
+// handle http requests
 const server = express();
 
-server.get('/graphql', (req, res) => {
-    res.send('<h1>Hello World</h1>')
-});
+// when we have mapped methods in argument, we just "use" them
+// this endpoint is our test graphql client
+server.use('/graphiql', graphiqlExpress({
+    // here we point to graphql server endpoint
+    endpointURL: "/graphql"
+}));
+
+// the graphql server is handled by apollo-server-express
+server.use('/graphql', bodyParser.json(), graphqlExpress({}));
 
 server.listen(4000, () => {
     console.log('Server listening on port 4000.');
